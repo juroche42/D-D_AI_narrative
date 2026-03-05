1fd436a530f88109bec13f7ba5c6a4380cfd0caf
thierrymgn <maignan.thierry@outlook.fr>
2026-03-05 14:04:23 +0100

    refactor: Remove Profile table and related constraints in migration

--- d-d_ai_narrative/prisma/migrations/20260303211613_add_room_models/migration.sql
+++ d-d_ai_narrative/prisma/migrations/20260303211613_add_room_models/migration.sql
@@ -1,23 +1,8 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "RoomStatus" AS ENUM ('WAITING', 'IN_PROGRESS', 'FINISHED');
 
 -- CreateEnum
 CREATE TYPE "ActionMode" AS ENUM ('VOTE', 'FREE');
-
--- DropForeignKey
-ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
-
--- DropTable
-DROP TABLE "Profile";
-
--- DropEnum
-DROP TYPE "ProfileLanguage";
 
 -- CreateTable
 CREATE TABLE "rooms" (
