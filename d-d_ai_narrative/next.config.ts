2fb05384b6e5f377af144b342d72bfd03b8a3576
Jules Roche <94986757+juroche42@users.noreply.github.com>
2026-09-18 15:05:35 +0200

    feat: add value for deploy (#278)

--- d-d_ai_narrative/next.config.ts
+++ d-d_ai_narrative/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   turbopack: {},
   serverExternalPackages: ["swagger-jsdoc", "@prisma/client", "pg", "bcrypt"],
 };
